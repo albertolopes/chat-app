ebadbf8e9678feb0cb02f789ea56e1a571678ecd
Alberto Lopes <cast.alberto@pe.sebrae.com.br>
2022-10-19 16:16:33 -0300

    Update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from "react";
 import ChatRoom from "./component/ChatRoom";
 
-function App() {
+const App = () => {
   return (
-    <ChatRoom></ChatRoom>
-  );
+    <ChatRoom />
+  )
 }
 
 export default App;
